3360376b7a1f0575eddaa7cab3700b92caf957b9
JNCamil <juancamvh@hotmail.com>
2024-02-08 22:15:59 +0100

    Generar pdf's

---
+++ generar_pdf/index.php
@@ -0,0 +1,44 @@
+<?php 
+//Tendremos que añadir el autoload que está en vendor, para tener disponibles los objetos y clases que hay en vendor
+require "../vendor/autoload.php";
+
+//Para utilizar la librería, cargamos el namespace. Cada librería está paquetizada, cada una tiene su namespace
+
+
+$html2pdf = new \Mpdf\Mpdf(); //Nos generará un pdf en base a un html y un css que pongamos 
+
+//Sin importarlo:
+//$html="<h1> hOLA MUNDO desde una librería de PHP para hacer PDF'S</h1>";
+//$html .= "<p>Master en PHP</p>";
+
+// Para importarlo desde otro lado
+// Recoger la vista para imprimir:
+ob_start();       //Recoge el html del archivo siguiente
+require "pdf_para_generar.php";
+$html = ob_get_clean();  //Aquí lo guardo
+
+
+//Podemos ver la documentación para ver sus métodos 
+
+//Escribir en el pdf:
+$html2pdf -> writeHTML ($html);
+//Para exportar todo ese html a un pdf:
+$html2pdf -> Output("pdf_generado.pdf");
+
+/*
+8 Feb 2024: Esta librería HTML2PDF da problemas con versiones modernas de PHP
+Debido a este problema: 
+Eliminar la disponibilidad de uso de llaves{} tanto para poder acceder a elementos de un vector como de un string offset, 
+con lo cual solo debe quedar el uso de [] como símbolo de acceso a los elementos.
+
+Para pasar de una sintaxis así**:
+
+$arr = [1, 2, 3];
+var_dump($arr{1});
+A una que será de ahora en adelante así:
+
+$arr = [1, 2, 3];
+var_dump($arr[1]);
+*/
+
+?>
---
+++ generar_pdf/pdf_para_generar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola MUNDO Generar PDF's</h1>
+    <p>Generando PDF'S con librerías</p>
+    <ul>
+        <li>Listas de ejemplo</li>
+        <li>Listas de ejemplo</li>
+        <li>Listas de ejemplo</li>
+        <li>Listas de ejemplo</li>
+        <li>Listas de ejemplo</li>
+    </ul>
+</body>
+</html>
