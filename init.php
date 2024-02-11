8d037a73e81ff873984e8d22caefbe322dc8a9db
JNCamil <juancamvh@hotmail.com>
2024-02-11 23:10:50 +0100

    review

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php 
+//Repositorio de paquetes y librerías de PHP: https://packagist.org/
+//Normalmente suele venir el comando para instalar la librería, 
+//Sólo tenermos que ir a la carpeta donde lo querramos y ejecutar el comando Nota*: Si hay espacios en la carpeta va entre comillas
+/*
+En composer.json es un objeto json donde trendremos el objeto con la dependencia que usaremos:
+{
+    "require": {
+        "spipu/html2pdf": "^4.6",
+        "dependencia o librería o paquete": "version" (Para añadir más)  //Si instalo otras librerías se añaden automáticamente
+    }
+}
+*/
+phpinfo();
+?>
