8bb1be99edd039ba4362b920917a2d9acea9ad86
JNCamil <juancamvh@hotmail.com>
2024-02-23 23:12:54 +0100

    review

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 //Normalmente suele venir el comando para instalar la librería, 
 //Sólo tenermos que ir a la carpeta donde lo querramos y ejecutar el comando Nota*: Si hay espacios en la carpeta va entre comillas
 /*
-En composer.json es un objeto json donde trendremos el objeto con la dependencia que usaremos:
+composer.json es un objeto json donde trendremos el objeto con las dependencias que usaremos:
 {
     "require": {
         "spipu/html2pdf": "^4.6",
